e2500d68fcc5ad895268a97cc0a0c5305a75cf44
Jason Nguyen Van <fahasoavana.nguyen@esti.mg>
2026-02-12 20:07:24 +0300

    maj 3

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./layout/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   template: `
-    <h1 class="text-2xl p-6">Welcome to {{ title() }}!</h1>
-
+    <app-header/>
+    <div class="h-[calc(100%-64px)] overflow">
     <router-outlet />
+    </div>
   `,
   styles: [],
 })
 export class App {
-  protected readonly title = signal('e-shopping');
+
 }
