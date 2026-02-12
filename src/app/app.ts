72a473181c48ac545798f4ef2a7dce8f783127a0
Jason Fahasoavana Nguyen Van <jnguyen@ulys.mg>
2026-02-12 09:50:04 +0300

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  template: `
+    <h1 class="bg-red-500">Welcome to {{ title() }}!</h1>
+
+    <router-outlet />
+  `,
+  styles: [],
+})
+export class App {
+  protected readonly title = signal('e-shopping');
+}
